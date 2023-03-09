772f347c4c2b0054e7bbb5895fbf625ccec6168e
20113320 <20113320@sigma.sbrf.ru@CAB-WSN-0036373.sigma.sbrf.ru>
2023-03-09 23:01:06 +0300

    All

---
+++ assets/js/player.js
@@ -0,0 +1,81 @@
+// модуль для работы с аудиоплеером
+const playerModule = (function() {
+  const audio = document.querySelector('audio');
+  const playButton = document.querySelector('.play-button');
+  let playing = false;
+
+  function playAudio() {
+    audio.play();
+    playing = true;
+    playButton.classList.add('playing');
+  }
+
+  function pauseAudio() {
+    audio.pause();
+    playing = false;
+    playButton.classList.remove('playing');
+  }
+
+  function toggleAudio() {
+    if (playing) {
+      pauseAudio();
+    } else {
+      playAudio();
+    }
+  }
+
+  return {
+    playAudio,
+    pauseAudio,
+    toggleAudio
+  }
+})();
+
+// модуль для работы со списком глав
+const chaptersModule = (function() {
+  const chapters = document.querySelectorAll('.chapters li a');
+
+  function scrollToChapter(chapterIndex) {
+    const chapterElement = chapters[chapterIndex];
+    const chapterOffsetTop = chapterElement.offsetTop - 100;
+    window.scrollTo({ top: chapterOffsetTop, behavior: 'smooth' });
+  }
+
+  function highlightCurrentChapter() {
+    const audioCurrentTime = Math.floor(playerModule.audio.currentTime);
+    for (let i = 0; i < chapters.length; i++) {
+      const chapterStartTime = parseInt(chapters[i].getAttribute('data-start-time'));
+      const chapterEndTime = parseInt(chapters[i].getAttribute('data-end-time'));
+      if (audioCurrentTime >= chapterStartTime && audioCurrentTime < chapterEndTime) {
+        chapters[i].classList.add('active');
+      } else {
+        chapters[i].classList.remove('active');
+      }
+    }
+  }
+
+  function bindChapterEvents() {
+    for (let i = 0; i < chapters.length; i++) {
+      const chapterStartTime = parseInt(chapters[i].getAttribute('data-start-time'));
+      chapters[i].addEventListener('click', function() {
+        playerModule.audio.currentTime = chapterStartTime;
+        playerModule.playAudio();
+        scrollToChapter(i);
+      });
+    }
+  }
+
+  return {
+    highlightCurrentChapter,
+    bindChapterEvents
+  }
+})();
+
+// инициализация плеера и списка глав
+function init() {
+  playerModule.audio.addEventListener('timeupdate', chaptersModule.highlightCurrentChapter);
+  chaptersModule.bindChapterEvents();
+  playerModule.playButton.addEventListener('click', playerModule.toggleAudio);
+}
+
+init();
