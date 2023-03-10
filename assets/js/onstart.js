54f6f40a6f68a4274ed9961d94064cd5022b8156
20113320 <20113320@sigma.sbrf.ru@CAB-WSN-0036373.sigma.sbrf.ru>
2023-03-10 20:07:36 +0300

    All

--- assets/js/onstart.js
+++ assets/js/onstart.js
@@ -5,25 +5,20 @@ async function setUrl(audio) {
 };
 
 async function loadAudio(url) {
-  // Fetch the MP3 file as an ArrayBuffer
   const response = await fetch(url);
   const arrayBuffer = await response.arrayBuffer();
 
-  // Convert the ArrayBuffer to a base64-encoded data URI
   const base64 = btoa(
     new Uint8Array(arrayBuffer)
       .reduce((data, byte) => data + String.fromCharCode(byte), '')
   );
   const dataURI = 'data:audio/mp3;base64,' + base64;
-
-  // Return the data URI
   return dataURI;
 }
 
-// Function to play an audio file
 function playAudio(dataURI, audio) {
-  // Create an audio element with the data URI as its source
   audio.src = dataURI;
+  audio.classList.remove('making');
 }
 
 window.onload = function() {
